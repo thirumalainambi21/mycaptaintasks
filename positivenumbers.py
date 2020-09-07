c3934e34fadbc79090e56866d2763c9246770ec3
thirumalainambi21 <70857198+thirumalainambi21@users.noreply.github.com>
2020-09-07 21:38:41 +0530

    Add files via upload

---
+++ positivenumbers.py
@@ -0,0 +1,11 @@
+list1=[12,-7,5,64,-14]
+print("list1= ",list1)
+for x in list1:
+	if x>0:
+		print(x,"",end="")
+list2=[12,14,-95,3]
+print("  list2=",list2) 
+
+for x in list2:
+	if x>0:
+		print(x,"",end="")
